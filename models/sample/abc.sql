cf348e8a8a497f7cd300e0ea2d474750204969a4
Akshay Gurav <akshaygurav10@gmail.com>
2026-03-20 02:58:11 +0000

    abc.sql

---
+++ models/sample/abc.sql
@@ -0,0 +1,5 @@
+select 'Akshay' as first_name, 'Gurav' as last_name, '1995-06-01' as DOB
+union all
+select 'Sairaj' as first_name, 'Gurav' as last_name, '1998-01-25' as DOB
+union all
+select 'Shubham' as first_name, 'Gurav' as last_name, '1997-03-05' as DOB
