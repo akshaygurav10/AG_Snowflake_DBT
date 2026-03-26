f4c233a18c2717a0e47bacf4214614927eb5572f
Akshay Gurav <akshaygurav10@gmail.com>
2026-03-26 03:37:03 +0000

    snapshots

---
+++ snapshots/snapshots_tickets.sql
@@ -0,0 +1,7 @@
+select 1 as ticket_id, 'new' as ticket_status, '2026-03-26' as updated_date
+union all
+select 2 as ticket_id, 'new' as ticket_status, '2026-03-26' as updated_date
+union all
+select 3 as ticket_id, 'in-progress' as ticket_status, '2026-03-26' as updated_date
+union all
+select 4 as ticket_id, 'in-progress' as ticket_status, '2026-03-26' as updated_date
